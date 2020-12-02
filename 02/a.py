2e963d5f6d54238e8754fa4b69b4643b468d8e45
Morgan Bentell <morgan.bentell@gmail.com>
2020-12-02 18:44:46 +0100

    day 2

---
+++ 02/a.py
@@ -0,0 +1,17 @@
+import re
+
+valid = 0
+
+with open('./a.in') as f:
+  for line in f.readlines():
+    match = re.search('(\d*)-(\d*) ([a-z]): ([a-z]*)', line)
+    min = int(match.group(1))
+    max = int(match.group(2))
+    char = str(match.group(3))
+    chars = str(match.group(4))
+    #print('min: {}, max: {}, char: {}, chars: {}'.format(min, max, char, chars))
+
+    if chars.count(char) >= min and chars.count(char) <= max:
+      valid += 1
+
+  print(valid)
---
+++ 02/b.py
@@ -0,0 +1,18 @@
+
+import re
+
+valid = 0
+
+with open('./a.in') as f:
+  for line in f.readlines():
+    match = re.search('(\d*)-(\d*) ([a-z]): ([a-z]*)', line)
+    p1 = int(match.group(1))
+    p2 = int(match.group(2))
+    char = str(match.group(3))
+    chars = str(match.group(4))
+    # print('p1: {}, p2: {}, char: {}, chars: {}'.format(p1, p2, char, chars))
+
+    if (chars[p1 - 1] is char) ^ (chars[p2 - 1] is char):
+      valid += 1
+
+  print(valid)
