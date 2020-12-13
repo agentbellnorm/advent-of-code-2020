3abcc31479408054d3e968ec08857123af47da19
Morgan Bentell <morgan.bentell@gmail.com>
2020-12-13 17:43:56 +0100

    i stole this solution. couldnt figure it out myself

--- 13/b.py
+++ 13/b.py
@@ -6,7 +6,7 @@ def departs(line, time):
     return (time + line[1]) % line[0] == 0
 
 
-with open("./13/test.in") as f:
+with open("./13/real.in") as f:
     start_time, notes = f.read().splitlines()
     start_time = int(start_time)
     bus_lines = []
@@ -14,8 +14,12 @@ with open("./13/test.in") as f:
         if line != "x":
             bus_lines.append((int(line), i))
 
-    first_line = bus_lines[0][0]
-    for time in itertools.count(0, first_line):
-        if all(map(lambda line: departs(line, time), bus_lines)):
-            print(time)
-            break
+    jump = i = bus_lines[0][0]
+
+    # this part is stolen from the internet 🤷🏼‍♂️
+    for b in bus_lines[1:]:
+        while (i + b[1]) % b[0] != 0:
+            i += jump
+        jump *= b[0]
+
+    assert 598411311431841 == i
