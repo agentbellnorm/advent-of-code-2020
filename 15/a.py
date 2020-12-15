20116cee219fdda7fc638ed7e4a4304a5c06f516
Morgan Bentell <morgan.bentell@gmail.com>
2020-12-15 07:25:33 +0100

    clean up a little

--- 15/a.py
+++ 15/a.py
@@ -5,21 +5,19 @@ def append_or_create(d, key, val):
         d[key] = [val]
 
 
+def init(spoken, starting_numbers):
+    for i, number in enumerate(starting_numbers):
+        spoken[number] = [i + 1]
+
+
 starting_numbers = [1, 20, 11, 6, 12, 0]
-# starting_numbers = [0, 3, 6]
-
 spoken = {}
-for i, number in enumerate(starting_numbers):
-    spoken[number] = [i + 1]
-
+init(spoken, starting_numbers)
 last_spoken = starting_numbers[-1]
 starting_turn = len(starting_numbers) + 1
 
-for turn in range(starting_turn, 30000000 + 1):
-    if last_spoken not in spoken:
-        spoken[last_spoken] = [turn]
-        last_spoken = 0
-    elif len(spoken[last_spoken]) == 1:
+for turn in range(starting_turn, 2020 + 1):
+    if last_spoken not in spoken or len(spoken[last_spoken]) == 1:
         last_spoken = 0
     else:
         prepre, pre = spoken[last_spoken][-2:]
@@ -27,4 +25,4 @@ for turn in range(starting_turn, 30000000 + 1):
 
     append_or_create(spoken, last_spoken, turn)
 
-assert 10652 == last_spoken
+assert 1085 == last_spoken
---
+++ 15/b.py
@@ -0,0 +1,28 @@
+def append_or_create(d, key, val):
+    if key in d:
+        d[key].append(val)
+    else:
+        d[key] = [val]
+
+
+def init(spoken, starting_numbers):
+    for i, number in enumerate(starting_numbers):
+        spoken[number] = [i + 1]
+
+
+starting_numbers = [1, 20, 11, 6, 12, 0]
+spoken = {}
+init(spoken, starting_numbers)
+last_spoken = starting_numbers[-1]
+starting_turn = len(starting_numbers) + 1
+
+for turn in range(starting_turn, 30000000 + 1):
+    if last_spoken not in spoken or len(spoken[last_spoken]) == 1:
+        last_spoken = 0
+    else:
+        prepre, pre = spoken[last_spoken][-2:]
+        last_spoken = pre - prepre
+
+    append_or_create(spoken, last_spoken, turn)
+
+assert 10652 == last_spoken
